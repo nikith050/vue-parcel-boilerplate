34b54d2bbf9c3b7beb706166653d672e223f0f94
Nikhith N <nikhith@geekyants.com>
2018-01-17 01:10:22 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# vue-parcel-boilerplate
-Vue + Parcel
+# Vue + Parcel
+
+Vue-Parel-Boilerplate is a simple Vue app setup using [parceljs](https://parceljs.org/), without the use of Webpack.
+
+# Installation
+
+```
+• git clone https://github.com/nikith050/vue-parcel-boilerplate.git
+
+• cd vue-parcel-boilerplate
+
+• yarn / npm install
+
+• yarn / npm start
+```
+
+Vue-Parel-Boilerplate requires node version >= 8
---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <div class="App">
+      <header class="App-header">
+        <img src="./logo.png" class="App-logo" alt="logo" />
+        <h1 class="App-title">Welcome to Vue</h1>
+      </header>
+      <p class="App-intro">
+        To get started, edit <code>src/App.vue</code> and save to reload.
+      </p>
+    </div>
+  </div>
+</template>;
+
+<script>
+export default {
+  // empty
+};
+</script>
+<style src="./App.css" />
